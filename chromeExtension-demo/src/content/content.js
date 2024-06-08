79831552029601a27e632696a0e61f1e17c5d2a5
zopeplone <115687693+zopeplone@users.noreply.github.com>
2024-06-09 01:38:58 +0800

    Delete chromeExtension-demo directory

--- chromeExtension-demo/src/content/content.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-export const knowLedgeKey = 'knowledge'
-const root = document.createElement('div')
-root.id = 'crxRoot'
-document.body.append(root)
-
-const app = createApp(App)
-app.mount(root)
